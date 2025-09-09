ba3c15737a2f0de502df02ec19cd31962b2506af
ChelseyB-UVA <qck2qg@virginia.edu>
2025-09-09 23:55:28 +0000

    Saving our two init files

---
+++ scripts/init_git_creds.sh
@@ -0,0 +1,11 @@
+!#/usr/bin/bash
+
+USER=qck2qg@virginia.edu
+NAME=ChelseyB-UVA
+
+git config --global --list
+
+git config --global user.email ${USER}
+git config --global user.name  ${NAME}
+
+git config --global --list
