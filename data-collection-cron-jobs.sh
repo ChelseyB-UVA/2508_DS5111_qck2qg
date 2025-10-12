c9900a22e5e5313d72e87792460bd9701f1bdcdf
ChelseyB-UVA <qck2qg@virginia.edu>
2025-10-12 12:58:24 -0400

    updated data-collection-cron-jobs.sh

--- data-collection-cron-jobs.sh
+++ data-collection-cron-jobs.sh
@@ -1,1 +1,3 @@
-cd home/ubuntu/2508_DS5111_qck2qg/; make clean; make wsjgainers_tstamp; make ygainers_tstamp
+31 9 * * 1-5 cd 2508_DS5111_qck2qg/; make clean; make wsjgainers_tstamp; make ygainers_tstamp; make clean;
+30 12 * * 1-5 cd 2508_DS5111_qck2qg/; make clean; make wsjgainers_tstamp; make ygainers_tstamp; make clean;
+01 16 * * 1-5 cd 2508_DS5111_qck2qg/; make clean; make wsjgainers_tstamp; make ygainers_tstamp; make clean;
